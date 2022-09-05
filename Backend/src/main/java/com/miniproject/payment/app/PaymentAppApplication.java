b7d1c25b6f9709088d217e0685845ac03146333c
chiragniyogi25 <65035998+chiragniyogi25@users.noreply.github.com>
2022-09-05 10:50:31 +0530

    Enabling BatchProcessing

--- Backend/src/main/java/com/miniproject/payment/app/PaymentAppApplication.java
+++ Backend/src/main/java/com/miniproject/payment/app/PaymentAppApplication.java
@@ -1,5 +1,6 @@
 package com.miniproject.payment.app;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,20 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = "com.miniproject.payment.app.config.BatchConfig,"
+//		+"com.miniproject.payment.app.controller,"
+//		+"com.miniproject.payment.app.listener,"+"com.miniproject.payment.app.config")
+
+@EnableBatchProcessing
 public class PaymentAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentAppApplication.class, args);
 	}
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurerAdapter() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("PUT", "DELETE",
-//						"GET", "POST").allowedHeaders("*");
-//			}
-//		};
-//	}
+
 
 }
